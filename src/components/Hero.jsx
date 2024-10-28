ed59169e680230914999845a5273bd16881cff49
Lavale1012 <lavale889@gmail.com>
2024-10-28 14:50:30 -0400

    added profile photo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,20 @@
+import image from "/public/profile.jpeg";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div
+      id="home"
+      className=" px-16 flex min-h-screen w-full items-center justify-center py-28 md:px-32 "
+    >
+      <div className="flex flex-col justify-center items-center gap-10 text-white">
+        <img
+          src={image}
+          alt=""
+          className=" w-[300px] h-[300px] rounded-full shadow-xl shadow-indigo-900 transition-all duration-300 hover:-translate-y-5 hover:scale-105 hover:shadow-2xl hover:shadow-indigo-600"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
