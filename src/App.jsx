ea3030cedf2f88c56b46fd2934ee5f6f7d29d12f
Lavale1012 <lavale889@gmail.com>
2024-10-28 13:43:44 -0400

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Tech from "./components/Tech";
+import Projects from "./components/Projects";
+import Contacts from "./components/Contacts";
+
 function App() {
-  return <div className=" text-cyan-700">Hello</div>;
+  return (
+    <div>
+      {/* <div className=" fixed -z-10 min-h-screen w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div> */}
+      <div className=" fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <main className=" flex flex-col items-center px-4 md:px-8 lg:px-16">
+        <Navbar />
+        <Hero />
+        <Tech />
+        <Projects />
+        <Contacts />
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,64 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { useState } from "react";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [IsOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!IsOpen);
+  return (
+    <nav className=" fixed top-0 z-10 flex w-full items-center justify-between border-b border-b-gray-700 bg-black/70 px-16 py-6 text-white backdrop-blur-md md:justify-evenly">
+      <a
+        href="#"
+        className=" bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent opacity-80 text-3xl font-semibold transition-all duration-300 hover:opacity-100"
+      >
+        Lavale
+      </a>
+      <ul className="hidden md:flex gap-10">
+        <a
+          href="#Home"
+          className=" cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+        >
+          <li>Home</li>
+        </a>
+        <a
+          href="#Tech"
+          className=" cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+        >
+          <li>Tech</li>
+        </a>
+        <a
+          href="#Projects"
+          className=" cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+        >
+          <li>Projects</li>
+        </a>
+        <a
+          href="#Contact"
+          className=" cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+        >
+          <li>Contact</li>
+        </a>
+      </ul>
+      <ul className="hidden md:flex gap-5">
+        <li>
+          <a
+            href="#github"
+            className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100 text-xl hover:text-orange-500"
+          >
+            <FaGithub />
+          </a>
+        </li>
+        <li>
+          <a
+            href="#LinkedIn"
+            className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100 hover:text-blue-800 text-xl"
+          >
+            <FaLinkedin />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
